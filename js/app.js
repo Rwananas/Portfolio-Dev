340a994f233ee5125b2f1ce1b40e8fc34ee003f1
erwannbruneteau <erwannthomas@gmail.com>
2023-02-16 23:50:00 +0100

    navbar et burger responsiv

--- js/app.js
+++ js/app.js
@@ -134,3 +134,20 @@ console.log(calcul(4, 5));
 
 
 // PORTFOLIO ---------------------------------------------------------
+
+function menuMobile() {
+    const btn = document.querySelector('.burger');
+    const header = document.querySelector('.header');
+    const links = document.querySelectorAll('.navbar a');
+
+    btn.addEventListener('click', () => {
+        header.classList.toggle('show-nav');
+    });
+    
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+        header.classList.remove('show-nav');
+        });
+    })
+}
+menuMobile();
